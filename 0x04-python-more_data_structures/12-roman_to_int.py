493ced6a6935808409113aac849bcb9efdb634f5
Marian-Bernice <marian.haligah@ashesi.edu.gh>
2022-08-31 06:53:29 -0700

    Revert "error fix"
    This reverts commit 5dcfbf91b29d6b62153ace34a28fea6dda37ab54.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string: str):
-     if roman_string is None or type(roman_string) != str:
-         return 0
-     data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-     numbers = [data[x] for x in roman_string] + [0]
-     rep = 0
+def roman_to_int(roman_string):
+    if roman_string is None or isinstance(roman_string, str) is False:
+        return 0
+    letter = {'I': 1, 'V': 5, 'X': 10,
+            'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    maxchar = 'I'
+    new_list = roman_string[::-1]
 
-    for i in range(len(numbers) - 1):
-        if numbers[i] >= numbers[i+1]:
-            rep += numbers[i]
+    for elm2 in new_list:
+        if letter[elm2] >= letter[maxchar]:
+            maxchar = elm2
+            total += letter[elm2]
         else:
-            rep -= numbers[i]
-
-    return rep
+            total -= letter[elm2]
+    return total
