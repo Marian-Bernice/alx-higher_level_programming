7130d08e81f5487fb15a17bd962b271253eecf03
Marian-Bernice <marian.haligah@ashesi.edu.gh>
2022-09-27 06:09:24 -0700

    error fix

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -8,5 +8,5 @@ import json
 
 
 def from_json_string(my_str):
-     """Returns an object from a json object string"""
-     return json.loads(my_str)
+    """Returns an object from a json object string"""
+    return json.loads(my_str)
