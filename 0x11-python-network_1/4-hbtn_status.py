a62b4d093bbc70247f0c3bc3eb37cfa1662afb61
Marian-Bernice <marian.haligah@ashesi.edu.gh>
2022-12-16 04:35:53 -0800

    error fixes

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,8 @@
 import requests
 
 
-if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
+if __name__ == '__main__':
+    r = requests.get("https://intranet.hbtn.io/status")
     print('Body response:')
-    print('\t- type:', type(r.text))
-    print('\t- content:', r.text)
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
